202093872cba98a776691c493708262e11e4d26e
Ayedh Gaser <alshmary_ad@yahoo.com>
2017-01-17 08:00:26 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,46 @@
+class SongsController < ApplicationController
+  def index
+    @songs=Song.all
+  end
+
+  def show
+    @song=Song.find(params[:id])
+  end
+
+  def new
+    @song=Song.new
+  end
+
+  def create
+    @song=Song.create(post_params(:title, :release_year, :released, :genre, :artist_name))
+    if @song.valid?
+      redirect_to songs_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song=Song.find(params[:id])
+  end
+
+  def update
+    @song=Song.find(params[:id])
+    if @song.update(post_params(:title, :release_year, :released, :genre, :artist_name))
+      redirect_to songs_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song=Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_path(@song)
+  end
+
+
+  def post_params(*args)
+    params.require(:song).permit(*args)
+  end
+end
